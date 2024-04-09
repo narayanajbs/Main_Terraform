3621e6dd66eb147f179ecaaf6a691434bb6e2eb8
narayanajbs <narayana.jbs.com>
2024-04-10 04:05:58 +0530

    added variables types

--- variableoverride/ec2.tf
+++ variableoverride/ec2.tf
@@ -1,3 +1,20 @@
+/* 1.	Commandline
+    Terraform plan –var=”instance_type=t3.medium”
+2.	–var-file
+    Terraform plan –var-file=”roboshop.tfvars”
+     Terraform apply –var-file=”roboshop.tfvars”
+ 
+3.	Terraform.tfvars
+We have to create terraform.tfvars file and defined value in that file
+> terraform plan 
+It will automatically read variables from terraform.tfvars and override the values
+    
+4.	ENV variables
+   Set TF_VAR_instance_type=t3.xlarge */
+
+
+
+
 resource "aws_instance" "roboshop" {
 
   ami           = var.ami_id
